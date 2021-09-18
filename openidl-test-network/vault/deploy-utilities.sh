98f7e4af088eddeab6d2b2e3d425a03c0995ba69
rajesh.sanjeevi <rajesh.sanjeevi@itpeoplecorp.com>
2021-09-18 01:36:33 -0400

    final test

--- openidl-test-network/vault/deploy-utilities.sh
+++ openidl-test-network/vault/deploy-utilities.sh
@@ -16,64 +16,65 @@ checkOptions() {
 }
 action() {
 echo "Install utilities"
-yum install unzip wget tar gzip jq which sed -y > /dev/null 2&>1
+yum install unzip wget tar gzip jq which sed -y
 result=$?
 if [ $result -ne 0 ]; then
-	echo "Failed to install utilities using yum install"
+        echo "Failed to install utilities using yum install"
     exit 1
 fi
 echo "Success with yum install for required utilities"
-wget https://get.helm.sh/helm-v3.7.0-rc.3-linux-amd64.tar.gz > /dev/null 2&>1
+wget https://get.helm.sh/helm-v3.7.0-rc.3-linux-amd64.tar.gz
 result=$?
 if [ $result -ne 0 ]; then
-	echo "Failed to download helm binary"
+        echo "Failed to download helm binary"
     exit 1
 fi
 echo "Downloaded helm binary"
-tar -zxvf helm-v3.7.0-rc.3-linux-amd64.tar.gz 1> /dev/null
+tar -zxvf helm-v3.7.0-rc.3-linux-amd64.tar.gz
 result=$?
 if [ $result -ne 0 ]; then
-	echo "Extracting helm binary failed"
+        echo "Extracting helm binary failed"
     exit 1
 fi
 echo "Helm binary extracted"
-mv linux-amd64/helm /usr/local/bin/helm 1> /dev/null
+mv linux-amd64/helm /usr/local/bin/helm
 result=$?
 if [ $result -ne 0 ]; then
-	echo "Failed to move helm binary under /usr/local/bin"
+        echo "Failed to move helm binary under /usr/local/bin"
     exit 1
 fi
 echo "Helm is ready for usage"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 result=$?
 if [ $result -ne 0 ]; then
-	echo "Failed to download awscli"
+        echo "Failed to download awscli"
     exit 1
 fi
 echo "awscli download completed"
 unzip awscliv2.zip
 result=$?
 if [ $result -ne 0 ]; then
-	echo "Extract awscli"
+        echo "Extract awscli"
     exit 1
 fi
 echo "awscli unzipped"
-openidl -- sh -c './aws/install'
+./aws/install --update
 result=$?
 if [ $result -ne 0 ]; then
-	echo "Failed to install awscli"
+        echo "Failed to install awscli"
     exit 1
 fi
 echo "awscli install completed"
-  for config in "(${ACCESS_ID}, ${SECRET_KEY}, ${REGION})"; do
-    aws configure set aws_access_key_id ${config}
-    rc=$?
-    if [ $rc -ne 0 ]; then
-      echo "Failed to set AWS env variable ${config}"
-      exit 1
-    fi
-    echo "${config} env variable is set"
-  done
+declare -a awskey=(${ACCESS_ID} ${REGION} ${SECRET_KEY})
+for config in "${awskey[@]}"; do
+  aws configure set aws_access_key_id ${config}
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    echo "Failed to set AWS env variable ${config}"
+    exit 1
+  fi
+  echo "${config} env variable is set"
+done
 }
 SECRET_KEY=""
 ACCESS_ID=""
